059e8fe34342894a92c3f73ee36c6366d5b7cd5c
Maraely <Mara.Bujor@stud.srh-campus-berlin.de>
2024-04-15 13:59:27 +0200

    update calculate.test.js

--- src/calculate.test.js
+++ src/calculate.test.js
@@ -1,12 +1,44 @@
+import {factorial} from './calculate';
+import {test, describe, expect, jest} from "@jest/globals"; // this is optional, all three are global variables im runner scope
 
-test ('0! is 1', () => {
-    expect(factorial(0)).toBe(1)
+
+describe('factorial', () => {
+
+    test('5! is 120', () => {
+        expect(factorial(5)).toBe(120)
+    });
+
+    test('0! is 1', () => {
+        expect(factorial(0)).toBe(1)
+    });
+
+    test('Factorial of negative int is throwing exception ', () => {
+        expect(() => {
+            factorial(-5);
+        }).toThrow();
+    });
+
+})
+
+describe('product', ()=>{
+
+    // product(5) --> 120
+    // product(1) --> 1
+
 });
 
-test ('Factorial of negative int is throwing exception', () => {
-    expect(()=> {
-        factorial(-5);
-    }).toThrow();
+//homework
+
+import { test, expect } from "@jest/globals";
+import { product } from './calculate';
+
+test('Product of terms from 1 to 5', () => {
+    const n = 5;
+    const expectedProduct = 1 * 2 * 3 * 4 * 5; // Product of terms from 1 to 5
+
+    const result = product(n);
+
+    expect(result).toBe(expectedProduct);
 });
 
-export
+
